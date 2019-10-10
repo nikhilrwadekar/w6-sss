3cea258348c491cb86258519318bf2dd085ed612
Nikhil Wadekar <ec2-user@ip-172-31-24-13.ec2.internal>
2019-10-10 21:53:21 +0000

    Add Row; Fetch and Print the row

--- db/setup.js
+++ db/setup.js
@@ -5,6 +5,7 @@ const { query } = require("./promise-mysql.js");
 const { cp } = require("./connection.js");
 
 // Using our DB promise
-query(cp, 'DROP TABLE IF EXISTS dogs;')
-    .then(() => { console.log('Dogs table dropped!') })
-    .catch(error => { console.log(error); });
+query(cp, 'DROP TABLE IF EXISTS dogs; CREATE TABLE dogs(name varchar(255));')
+    .then(result => query(cp, "INSERT INTO dogs(name) VALUES ('Scuffles');"))
+    .then(result => query(cp, "SELECT * FROM dogs;"))
+    .then(result => console.log(result.name))
