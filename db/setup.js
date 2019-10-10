f1e34d325155a981bdeb355e5ba0185d221741b6
Nikhil Wadekar <ec2-user@ip-172-31-24-13.ec2.internal>
2019-10-10 22:00:31 +0000

    Final Changes

--- db/setup.js
+++ db/setup.js
@@ -8,4 +8,6 @@ const { cp } = require("./connection.js");
 query(cp, 'DROP TABLE IF EXISTS dogs; CREATE TABLE dogs(name varchar(255));')
     .then(result => query(cp, "INSERT INTO dogs(name) VALUES ('Scuffles');"))
     .then(result => query(cp, "SELECT * FROM dogs;"))
-    .then(result => console.log(result.name))
+    .then(result => console.log(result))
+    .then(() => { process.exit(); })
+    .catch(error => console.log(error))
